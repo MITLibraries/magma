b72175f1cca67fb7d6b7a7a6531bdaddd6e28de0
Jeremy Prevost <jprevost@mit.edu>
2015-10-27 10:52:04 -0400

    Validate form inputs
    closes #2

--- magma/app.py
+++ magma/app.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import os
 from flask import Flask
 from magma.home import home_page
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.update(SECRET_KEY=os.environ.get('SECRET_KEY', 'changeme'))
     register_blueprints(app)
     return app
 
---
+++ tests/test_views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import pytest
+
+from webtest import TestApp, Upload
+from magma.app import create_app
+
+def test_can_load_index(testapp):
+    r = testapp.get('/')
+    assert r.status_code == 200
+    assert 'form' in r
+
+def test_submit_valid_form(testapp):
+    form = testapp.get('/').form
+    form['data'] = Upload('tests/fixtures/SDE_DATA_BD_A8GNS_2003.zip')
+    form['metadata'] = Upload('tests/fixtures/fgdc.xml')
+    res = form.submit()
+    assert res.content_type == 'text/xml'
+
+def test_submit_with_shapefile_without_metadata(testapp):
+    form = testapp.get('/').form
+    form['data'] = Upload('tests/fixtures/SDE_DATA_BD_A8GNS_2003.zip')
+    res = form.submit()
+    assert res.content_type == 'text/xml'
+
+def test_submit_without_shapefile(testapp):
+    form = testapp.get('/').form
+    res = form.submit()
+    assert 'The shapefile is required to proceed' in res
+
+def test_submit_with_invalid_shapefile(testapp):
+    form = testapp.get('/').form
+    form['data'] = Upload('tests/fixtures/fgdc.xml')
+    res = form.submit()
+    assert 'File is not a zip file' in res
